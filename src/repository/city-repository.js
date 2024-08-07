db7677b27c9a0eff19af7a9ade2be2ad8d396f8f
Titiksha2608 <titikshamuk@gmail.com>
2024-08-07 10:46:09 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,4 +1,6 @@
 const {City} = require(`../models/index`);
+//responsible for communicating with any data source can be db or from somewhere else we r fetching
+//fetching the data not creating any issue
 
 class CityRepository{
     async createCity({name}){
@@ -6,7 +8,8 @@ class CityRepository{
             const city = await City.create({name});
             return city;
         }catch (error){
-        throw{error};
+        console.log("Something went wrong in the repository layer");
+        throw {error};
     }
 }
     async deleteCity(cityId){
@@ -16,7 +19,32 @@ class CityRepository{
                     id: cityId
                 }
             });
+            return true;
         }catch(error) {
+            console.log("Something went wrong in the repository layer");
+            throw{error};
+        }
+    }
+
+    async updateCity(cityId,data){
+        try {
+            const city = await City.update(data,{
+                where:{
+                    id : cityId
+                }
+            });
+                 return city;   
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw{error};
+        }
+    }
+    async getCity(cityId){
+        try {
+            const city = await City.findByPk(cityId);//does query on primary key
+            return city;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
             throw{error};
         }
     }
